a59a40a247ab37c02168b54ecdae246a29b210fe
Kjetil <kjetil_gulli@live.no>
2018-08-28 11:12:04 +0200

    asd

--- p1_vel_dist.py
+++ p1_vel_dist.py
@@ -6,6 +6,16 @@ import prob_dist as pd
 maxwell boltzmann
 p(v) = (m/2pikT)**3/2*e**(-1/2*mv**2/kT)
 --> black box --> sig = sqrt(kT/m), mu = 0
+
+k =  1.380 648 52 x 10-23 J K-1
+https://physics.nist.gov/cgi-bin/cuu/Value?k|search_for=boltzmann
+
+Na = 6.022 140 857 x 1023 mol-1
+https://physics.nist.gov/cgi-bin/cuu/Value?na|search_for=avogadro
+
+
+mass H_2 = 2.016 g/mol
+https://pubchem.ncbi.nlm.nih.gov/compound/Hydrogen
 '''
 
 molmassH2 = 2.01588 #g/mol
