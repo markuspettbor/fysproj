3cda46d9ecd15c8c71b8c02022781b12272f3a96
Markus <markus_er_nr3@hotmail.com>
2018-08-28 23:51:34 +0200

    made a rectangle!

--- physicals.py
+++ physicals.py
@@ -25,23 +25,29 @@ class Wall:
     def __call__(self):
         print('wall coordinates returned')
 
-    def determine_corners(self):
-        pass
+    def get_corners(self):
+        '''
+        Finds corners in a rectangle of width w, height h, for a given
+        normal_vector. Idea: Find a vector in the plane, cross that vector
+        with the normal vector, and get a vector perpendicular to the vector
+        in the plane. The two can then be used to span a rectangle.
 
-    def get_plane(self):
+        For future: Should redo entire code. Should also add option
+        to build a wall from an existing one.
+        '''
         a, b, c = self.normal_vector
         x0, y0, z0 = self.position
         p = np.array([0, 0, a/c*x0 + b/c*y0 + z0])
         tryvec = p - self.position
         othervec = np.cross(tryvec, self.normal_vector)
-        res1 = tryvec/np.linalg.norm(tryvec)*self.w + othervec*np.linalg.norm(othervec)/self.h
-        res2 = tryvec/np.linalg.norm(tryvec)*self.w
-        res3 = tryvec/np.linalg.norm(tryvec)*self.h
-        for res in (res1, res2, res3):
-            print('Uno ', res + self.position)
-        print(self.position)
+        res1 = self.position + tryvec/np.linalg.norm(tryvec)*self.w + othervec/np.linalg.norm(othervec)*self.h
+        res2 = self.position + tryvec/np.linalg.norm(tryvec)*self.w
+        res3 = self.position + othervec/np.linalg.norm(othervec)*self.h
+        return res1, res2, res3, self.position
+
 
 def build_the_wall():
     wall = Wall(10, 5, np.array([0, 0, 1]), np.array([1,1,1]))
-    wall.get_plane()
+    wall.get_corners()
+
 build_the_wall()
