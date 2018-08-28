80503e5d13e7b687b08f5c1f06f3df958f1f6695
Markus <markus_er_nr3@hotmail.com>
2018-08-28 09:56:58 +0200

    Changed file structure (added physicals.py)

--- gas.py
+++ gas.py
@@ -1,21 +1,5 @@
 import numpy as np
 
-class Gas:
-    def __init__(self, num_particles, temperature, positions):
-        self.num_particles = num_particles
-        self.temperature = temperature
-        self.positions = positions
-
-    def addParticles(self, new_particles, mass):
-        pass
-
-class Wall:
-    def __init__(self):
-        pass
-
-    def __call__(self):
-        print('wall coordinates returned')
-
 class Engine:
     def __init__(self, length, width, height):
         pass
---
+++ physicals.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+class Gas:
+    def __init__(self, num_particles, temperature, positions, radius  = 0):
+        self.num_particles = num_particles
+        self.temperature = temperature
+        self.positions = positions
+
+    def addParticles(self, new_particles, mass, radius = 0):
+        pass
+
+class Wall:
+    def __init__(self):
+        pass
+
+    def __call__(self):
+        print('wall coordinates returned')
