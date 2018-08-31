c6769b09c102d41afee4c3c8e840856953fa613a
Markus <markus_er_nr3@hotmail.com>
2018-08-31 15:13:45 +0200

    revamped prob_dist and physicals to be more readable

--- physicals.py
+++ physicals.py
@@ -1,14 +1,22 @@
 import numpy as np
 import numtools as nt
+import prob_dist as pd
+
+import matplotlib.pyplot as plt
 
 class Gas:
-    def __init__(self, num_particles, temperature, positions, radius  = 0):
+    def __init__(self, num_particles, temperature, mass = 0, radius  = 0):
         self.num_particles = num_particles
         self.temperature = temperature
-        self.positions = positions
+        self.position = np.zeros((1, 3))
+        self.velocity = np.zeros((1, 3))
+        self.addParticles(num_particles, mass, radius)
 
-    def addParticles(self, new_particles, mass, radius = 0):
-        pass
+    def addParticles(self, new, mass, radius = 0):
+        boltzmax = pd.BoltzmannMaxwell()
+        np.append(self.position, np.random.uniform(size = (new, 3)))
+        np.append(self.velocity, boltzmax.distribution(size = (new, 3) ) )
+        print(self.velocity)
 
 class Wall:
     def __init__(self, w, h, normal_vector, position, neighbours = None):
@@ -30,8 +38,10 @@ class Wall:
         pass
 
 
+def test():
+    h2 = Gas(10, 5000)
+    print(h2.num_particles)
+    plt.plot(h2.velocity)
+    plt.show()
 
-def build_the_wall():
-    pass
-
-build_the_wall()
+test()
