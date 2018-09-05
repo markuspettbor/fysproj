59ded916cd18d8a9bb8fa912daea91d8a1080212
kjetil-desktop <kjetil_gulli@live.no>
2018-09-05 21:52:09 +0200

    finpussa og fiksa litt på thrust og fuel og fant verdier som jeg håper gir mening

--- rocket_parts.py
+++ rocket_parts.py
@@ -23,12 +23,7 @@ class Engine:
         force = delta_momentum/delta_time
         return force, fuel_used
 
-    def boost(self, thrust, consumption, initial_mass, delta_speed_desired, dt = 0.01):
-        mass = initial_mass
-        delta_speed = 0; a = 0
-        while delta_speed < delta_speed_desired:
-            delta_speed, mass = nt.euler_fuel_consumption(delta_speed, mass, thrust, consumption, dt)
-        return initial_mass - mass
+
     '''
     can either return force, or save force as self.force and call
     engine to get the force
--- test_tools.py
+++ test_tools.py
@@ -5,6 +5,7 @@ import pygame as pg
 import random
 import numpy as np
 import rocket_parts as rp
+import prototypes
 
 class Screen(object):
     def __init__(self, w, h):
@@ -21,56 +22,59 @@ class Screen(object):
 
 if __name__ == '__main__':
     width = 800 # Can't remember resolution things
-    height = 600
+    height = 800
     test = Screen(width, height)
     testrun = True
     surf = pg.display.get_surface()
 
+    box_width = 1e-6
     x0 = 0
-    y0 = int(width/2)
-    z0 = int(height/2)
-    box_width = 400
+    y0 = box_width#int(width/2)
+    z0 = box_width#int(height/2)
 
-    h2 = ph.Gas(1000, 3000, -box_width/2, box_width/2)
+    h2 = ph.Gas(50000, 3000, -box_width/2, box_width/2)
     p = h2.position.transpose() + np.array([0, y0, z0])
     p = p.transpose()
     v = h2.velocity
 
     # Alternative box:
-    import prototypes
+
 
     bigbox = prototypes.Box(x0, y0, z0 , box_width, hole_index = 4, hole_width = box_width/2)
     walls = bigbox.walls
 
-    dt = 0.0001
+    dt = 1e-12 #må stemme overens med dimensjoner på box og hastigheter
     white = (255, 255, 255, 255)
     count = 0
     force = 0
     fuel_used = 0
     tot_force = 0
     engine = rp.Engine(1000,100)
+    intervals = 1e3
+
+
     while testrun:
         for wall in walls:
-            pg.draw.circle(surf, white, (int(wall.center[1]), int(wall.center[2])), 2)
+            #pg.draw.circle(surf, white, (int(wall.center[1]), int(wall.center[2])), 2)
             v = wall.check_collision(p, v)
-            force_temp, fuel_used_temp = engine.particles_escaped(wall.escaped_velocity, \
+            force_temp, fuel_used_temp = engine.particles_escaped(wall.escaped_particles, \
                                              wall.escaped_velocity, dt)
             force += force_temp; fuel_used += fuel_used_temp
             wall.reset_escaped_particles()
-            #print(p)
+
         v, p = nt.euler_cromer_simple(p, v, dt)
         if count % 100 == 0:
             for x, y in zip(p[1], p[2]):
-                pg.draw.circle(surf, white, (int(x), int(y)), 0)
+                pg.draw.circle(surf, white, (int(x*400/box_width), int(y*400/box_width)), 0)
             pg.display.flip()
             surf.fill((0,0,0))
         count += 1
         #print(force)
         tot_force += force
         force = 0
-        if count % int(1/dt) == 0:
-            print(tot_force)
-            print(fuel_used)
+        if count % int(intervals) == 0:
+            print('Force %19.5e [N]' % (tot_force/intervals))
+            print('Fuel consumed %11.5e [1/s]' % (fuel_used/intervals/dt))
             tot_force = 0
             fuel_used = 0
         for event in pg.event.get():
