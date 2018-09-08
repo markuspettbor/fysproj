e24322f396a545ab07f2d68c0f48683c43da47b6
Markus <markus_er_nr3@hotmail.com>
2018-09-09 01:30:31 +0200

    zzz

--- part2.py
+++ part2.py
@@ -5,12 +5,8 @@ import numtools as nt
 import prettyplot as pretty
 import variables as vars
 
-user = 'markusbp'
-
-seed = AST2000SolarSystem.get_seed(user)
-solar_system = AST2000SolarSystem(seed)
-
-
+user = vars.seed
+solar_system = vars.solar_system
 n = vars.n
 x0 = vars.x0
 y0 = vars.y0
@@ -19,6 +15,7 @@ vy0 = vars.vy0
 a = vars.a
 e = vars.e
 theta0 = vars.theta0
+psi0 = vars.psi0
 radius = vars.radius
 m_star = vars.m_star
 m = vars.m
@@ -28,26 +25,46 @@ def gravity(m1, m2, x):
     #    print(G**m1*m2/nt.norm(x)**3*x)
     return -G*m1*m2/nt.norm(x)**3*x
 
+def rotate(vector, angle):
+    # Rotation matrix
+    x1 = np.cos(angle)
+    x2 = np.sin(angle)
+    rotmatrix = np.array([[x1, -x2], [x2, x1]])
+    return np.dot(vector, rotmatrix)
+
 acc = lambda x, t: gravity(m_star, m[0], x)/m[0]
 
-for a, e, theta0, x0, y0, rad, vx0, vy0 in zip(a, e, theta0, x0, y0, radius, vx0, vy0 ):
-    theta = np.linspace(theta0, theta0 + 2*np.pi, 100000)
-    area = np.log(2*np.pi*(rad)**2)**2/30
-    r = a*(1 - e**2)/(1 + e*np.cos(theta))
-    x = r*np.cos(theta)
-    y = r*np.sin(theta)
-    plt.plot(x, y, linewidth = 0.8)
-    plt.scatter(x0, y0, s = area)
-    plt.axis('equal')
+for theta, psi, a, e, x0, y0, vx0, vy0 in zip(theta0, psi0, a, e, x0, y0, vx0, vy0):
+    angle = np.linspace(theta, theta + 2*np.pi, 1000)
+    r = a*(1-e**2)/(1 + e*np.cos(angle))
+    x = np.array([r*np.cos(angle), r*np.sin(angle)]).transpose()
 
-    time = np.linspace(0, 10,  20000)
-
+    x = rotate(x, psi + np.pi)
+    plt.plot(x[:, 0], x[:,1])
+    plt.scatter(x0, y0)
+    time = np.linspace(0, 10,  100000)
     initial_position = np.array([x0, y0])
     initial_velocity = np.array([vx0, vy0])
     xx, vv = nt.leapfrog(initial_position, initial_velocity, time, acc)
     plt.plot(xx[:,0], xx[:,1], linewidth = 0.8)
-    plt.axis('equal')
-plt.xlabel('x'); plt.ylabel('y')
 
-plt.title('Planetary Orbits')
+    plt.axis('equal')
+    plt.xlabel('x'); plt.ylabel('y')
+    plt.title('Planetary Orbits')
 plt.show()
+
+'''
+This is a test for the calculated radius (from ellipse formula) vs. sqrt(x0**2 + y0**2)
+Seems that it's not gonna get much closer...
+Decided that the easiest way to implement an orbit of any kind was to simply
+rotate an ellipse from the origin by the inclination psi.
+
+r_calc               r_assignment
+0.34088881931000203 0.3475322418676838
+0.46245543984259346 0.46628905945120847
+3.136171444566258 3.2266464987667134
+1.9934623737098005 1.9876202428208163
+0.21420545424335802 0.21626184615601463
+1.1235659125452286 1.125812221107684
+0.8821933895378447 0.8816067550484565
+'''
--- variables.py
+++ variables.py
@@ -21,6 +21,7 @@ vy0 = solar_system.vy0
 a = solar_system.a
 e = solar_system.e
 theta0 = solar_system.omega
+psi0 = solar_system.psi
 radius = solar_system.radius #radius of planets as list/array
 m_star = solar_system.star_mass
 m = solar_system.mass #mass of planets as list/array
@@ -31,7 +32,9 @@ radius_normal_unit = radius*1000
 radius_AU = radius_normal_unit / AU_tall
 satellite = solar_system.mass_sat
 
+'''
 solar_system.engine_settings(2.558e-10, 6.566e15, 1.742e13, 115e3, \
 4.136*60, np.array([x0[0] + radius_AU[0], 0]), 0)
 final_launch_pos = np.array([x0[0] + radius[0] + 6.232e6/AU_tall, 0])
 solar_system.mass_needed_launch(final_launch_pos, test = True)
+'''
