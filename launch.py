146bf9a9e07a07d659ea5b398305e889269ea529
kjetil <kjetil_gulli@live.no>
2018-09-08 18:46:30 +0200

    launch varialbes

--- launch.py
+++ launch.py
@@ -3,25 +3,29 @@ import variables
 def launch(force_box, fuel_box):
     radius = variables.radius_normal_unit[0]
     planet_mass = variables.m_normal_unit[0]
-    print('Planet Mass =', planet_mass)
-    print('Plannet Radius =', radius)
     grav_const = variables.gravitational_constant
+    satellite_mass = variables.satellite
     position = radius
+    rocket_mass = 0
+    initial_mass = 200e3
     force = 1680e3
     boxes = force/force_box
     fuel_consumption = boxes * fuel_box
-    initial_mass = 200e3
-    mass = initial_mass
-    print('g =',grav_const*planet_mass/(position**2))
+    fuel_mass = 100e3
+    mass = satellite_mass + fuel_mass + rocket_mass
     dt = 0.01
     t = 0
     escape_velocity = (2*grav_const*planet_mass/position)**0.5
-    print('Escape Velocity at Surface =', escape_velocity)
     velocity = 0
     count = 0
-    print('Initial Mass of Rocket =' , initial_mass)
+    has_fuel = 1
+    print('Escape Velocity at Surface = %.3e' % escape_velocity)
+    print('Planet Mass = %.3e' % planet_mass)
+    print('Plannet Radius = %.3e' % radius)
+    print('g =',grav_const*planet_mass/(position**2))
+    print('Initial Mass of Rocket = %.3e' % initial_mass)
     while velocity < escape_velocity: #1Dimentional
-        acceleration = force/mass - grav_const*planet_mass/(position**2)
+        acceleration = force/mass*has_fuel - grav_const*planet_mass/(position**2)
         #print(acceleration)
         velocity = velocity + acceleration*dt
         position = position + velocity*dt
@@ -30,12 +34,14 @@ def launch(force_box, fuel_box):
         if count % 100 == 0:
             plt.scatter(t, acceleration)
         escape_velocity = (2*grav_const*planet_mass/position)**0.5
+        if fuel_mass < 0:
+            has_fuel = 0
         t = t + dt
         count = count+1
-    print('Final Mass of Rocket =', mass)
-    print('Fuel Percentage Left =', mass/initial_mass)
-    print(t/60)
-    print('Final Position =', position)
-    print('Final Velocity =', velocity)
-    print('Boxes Used =', boxes)
+    print('Final Mass of Rocket = %.3e' % mass)
+    print('Fuel Percentage Left = %.3f' % (100*mass/initial_mass))
+    print('Launch Time = %.3f Minutes' % (t/60))
+    print('Final Position = %.3e' % position)
+    print('Final Velocity = %.3e' % velocity)
+    print('Boxes Used = %.3e' % boxes)
     plt.show()
--- variables.py
+++ variables.py
@@ -25,8 +25,13 @@ radius = solar_system.radius #radius of planets as list/array
 m_star = solar_system.star_mass
 m = solar_system.mass #mass of planets as list/array
 G = 4*np.pi**2
+AU_tall = 149597871000
 m_normal_unit = m*1.989e30
 radius_normal_unit = radius*1000
+radius_AU = radius_normal_unit / AU_tall
+satellite = solar_system.mass_sat
 
-satelite = solar_system.mass_sat
-print(satelite)
+solar_system.engine_settings(2.558e-10, 6.566e15, 1.742e13, 115e3, \
+4.136*60, np.array([x0[0] + radius_AU[0], 0]), 0)
+final_launch_pos = np.array([x0[0] + radius[0] + 6.232e6/AU_tall, 0])
+solar_system.mass_needed_launch(final_launch_pos, test = True)
