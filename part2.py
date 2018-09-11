07ad2257e6f5fe6944801ec228edbf748209ae5d
Markus <markus_er_nr3@hotmail.com>
2018-09-11 14:53:20 +0200

    Redid part2 syntax to be simpler and more flexible

--- part2.py
+++ part2.py
@@ -2,10 +2,27 @@ from AST2000SolarSystem import AST2000SolarSystem
 import matplotlib.pyplot as plt
 import numpy as np
 import numtools as nt
-import prettyplot as pretty
 import variables as vars
 
-user = vars.seed
+def gravity(m1, m2, x):
+    return -G*m1*m2/nt.norm(x)**3*x
+
+def kepler3(m1, m2, a):
+    return np.sqrt(4*np.pi**2/(G*(m1+m2))*a**3)
+
+def orbit(x0, v0, acc, t0, t1, steps):
+    '''
+    Assumes x0, v0, are arrays containing [x0, y0], [vx0, vy0]
+    acc is a function defining the acceleration affecting
+    the body to be simulated.
+    returns x (orbital position) and v (orbital velocity)
+    '''
+    time = np.linspace(t0, t1, steps)
+    x, v = nt.leapfrog(x0, v0, time, acc)
+    return x, v, time
+
+
+
 solar_system = vars.solar_system
 n = vars.n
 x0 = vars.x0
@@ -21,35 +38,62 @@ m_star = vars.m_star
 m = vars.m
 G = vars.G
 
-k = 1000
+# Analytical solution
+k = 100
 thetas = np.array([np.linspace(theta, theta + 2*np.pi, k) for theta in theta0])
 thetas = np.transpose(thetas)
 r = a*(1-e**2)/(1 + e*np.cos(thetas- (np.pi + psi0)))
-x = np.array([r*np.cos(thetas), r*np.sin(thetas)])
-plt.plot(x[0], x[1], '-.r')
+x_analytical = np.array([r*np.cos(thetas), r*np.sin(thetas)])
 
-def gravity(m1, m2, x):
-    return -G*m1*m2/nt.norm(x)**3*x
+# Numerical solution
 
-acceleration = np.array()
+orbits = 20
+stepsperorbit = 10000
+period = kepler3(m_star, m, a)
+t0 = 0
+t1 = orbits*period
+steps = t1*stepsperorbit
 
-for x0, y0, vx0, vy0, m in zip(x0, y0, vx0, vy0, m):
-    acceleration = lambda x, t: gravity(m_star, m, x)/m
+orbital_x = []
+orbital_y = []
+velocity_vx = []
+velocity_vy = []
+all_time = []
+
+for m, p, index, t1, step in zip(m, period, range(n), t1, steps):
+    acc = lambda r, t: gravity(m_star, m, r)/m
+    period = kepler3(m_star, m, a)
+    initial_x = np.array([x0[index], y0[index]])
+    initial_v = np.array([vx0[index], vy0[index]])
+    x, v, t = orbit(initial_x, initial_v, acc, t0, t1, step)
+    orbital_x.append(x[0])
+    orbital_y.append(x[1])
+    velocity_vx.append(v[0])
+    velocity_vy.append(v[1])
+    all_time.append(t)
+
+for x,y in zip(orbital_x, orbital_y):
+    plt.plot(x, y, linewidth = 0.6)
     plt.scatter(x0, y0)
-    time = np.linspace(0, 0.1,  10000)
-    initial_position = np.array([x0, y0])
-    initial_velocity = np.array([vx0, vy0])
-    xx, vv = nt.leapfrog(initial_position, initial_velocity, time, acceleration)
-    plt.plot(xx[:,0], xx[:,1], linewidth = 0.8)
     plt.axis('equal')
-    plt.xlabel('x'); plt.ylabel('y')
-    plt.title('Planetary Orbits')
+plt.plot(x_analytical[0], x_analytical[1], '-.r',linewidth = 0.8)
+plt.xlabel('x'); plt.ylabel('y')
+plt.title('Planetary Orbits')
 plt.show()
 
 '''
+acceleration = lambda x, t: gravity(m_star, m, x)/m
+time = np.array([np.linspace(0, period, k*period) for period in range(1, n+1)])
+initial_position = np.array([x0, y0])
+initial_velocity = np.array([vx0, vy0])
+xx, vv = nt.leapfrog(initial_position, initial_velocity, time, acceleration)
+plt.plot(xx[:, 0], xx[:,1])
+plt.show()
+
 angle = np.linspace(theta, theta + 2*np.pi, 1000)
 r = a*(1-e**2)/(1 + e*np.cos(angle - (np.pi + psi)))
 x = np.array([r*np.cos(angle), r*np.sin(angle)])
 plt.plot(x[0], x[1], '.g')
 plt.scatter(x0, y0)
+
 '''
--- physicals.py
+++ physicals.py
@@ -63,5 +63,35 @@ class Wall():
         self.escaped_particles = 0
         self.escaped_velocity = 0
 
+
+class Box:
+    def __init__(self, dx, dy, dz, width, hole_index = 5, hole_width = 200):
+        '''
+        Creates the simplest of all boxes.
+        '''
+        normals = np.array([[1,0,0], [-1, 0, 0], [0, 1, 0],\
+                            [0, -1, 0], [0, 0, 1], [0, 0, -1]])
+        axes = np.array([0, 0, 1, 1, 2, 2])
+        signs = np.array([1, -1, 1, -1, 1, -1])
+        x1 = np.array([dx + width/2, dy, dz])
+        x2 = np.array([dx - width/2, dy, dz])
+        x3 = np.array([dx, dy + width/2, dz])
+        x4 = np.array([dx, dy - width/2, dz])
+        x5 = np.array([dx, dy, dz + width/2])
+        x6 = np.array([dx, dy, dz - width/2])
+        centers = [x1, x2, x3, x4, x5, x6]
+        self.walls = []
+        holes = np.zeros(6)
+        holes[hole_index] = hole_width
+        self.make_box(normals, axes, signs, centers, holes)
+
+    def add_wall(self, normal, axis, sign, center, hole):
+        return Wall(normal, axis, sign, center, hole_width = hole)
+
+    def make_box(self, norms, axes, signs, centers, holes):
+        for n, ax, sign, cntr, hole in zip(norms, axes, signs, centers, holes):
+            self.walls.append(self.add_wall(n, ax, sign, cntr, hole))
+
+
 if __name__ == '__main__':
     print('main')
