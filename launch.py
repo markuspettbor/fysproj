5b8162491a414c5c4d1cb7a3210db84950febabc
kjetil <kjetil_gulli@live.no>
2018-09-11 12:01:50 +0200

    launch, test tools

--- launch.py
+++ launch.py
@@ -14,7 +14,7 @@ def launch(force_box, fuel_box, testing = False):
     fuel_mass = 80e3 #input? regne ut?
     dt = 0.01 #input?
     t0 = 0 #input?
-    phi = 2*np.pi/(period*24*60*60)
+    phi = 0*np.pi/(period*24*60*60)
     theta = 0*np.pi
     boxes = force/force_box
     fuel_consumption = boxes * fuel_box
@@ -55,24 +55,26 @@ def launch(force_box, fuel_box, testing = False):
     print('Boxes Used = %.3e' % boxes)
     plt.show()
 
-    if testing == True:
-        variables.solar_system.engine_settings(force_box, boxes, part_consumed_box, initial_fuel_mass, \
-        t, np.array([variables.x0[0] + variables.radius_AU[0], 0]), 0)
-        final_launch_pos = np.array([variables.x0[0] + variables.radius_AU[0] + position/variables.AU_tall, 0])
-        variables.solar_system.mass_needed_launch(final_launch_pos, test = True)
-
     #solar system frame of reference
     #rotasjon om planeten med vinkel phi = 2*np.pi/(period*24*60*60)
     #launchsite theta = vinkel til launchsite
     #position_planet, velocity_planet = orbit_calculation(t0, t) #where t0 is start of launch and t is launch duration
         #returnerer ny position til planet om sola, og hastighet om sola
+    print([position, 0])
+    phi = t*np.pi/(period*24*60*60)
     position_rs = nt.rotate(np.array([position,0]).transpose(), phi + theta) #returns array with [x, y]
     velocity_rs = nt.rotate(np.array([velocity,0]).transpose(), phi + theta)
     print(position_rs)
     print(velocity_rs)
-    position_final = position_planet + position_rs # position_planet given from orbital calculation
+    position_planet, velocity_planet = np.array([variables.x0[0], variables.y0[0]]), np.array([variables.vx0[0], variables.vy0[0]]) #where t0 is start of launch and t is launch duration
+    position_final = position_planet*variables.AU_tall + position_rs # position_planet given from orbital calculation
     velocity_final = velocity_planet + velocity_rs # position_planet given from orbital calculation
     #assume all previous is given in SI units
     position_final_AU = position_final/variables.AU_tall #AU
     velocity_final_AU = velocity_final*60*60*24*365/variables.AU_tall #AU/year
     time_AU = t/(60*60*24*365)
+
+    if testing == True:
+        variables.solar_system.engine_settings(force_box, boxes, part_consumed_box, initial_fuel_mass, \
+        t, nt.rotate(np.array([variables.x0[0] + variables.radius_AU[0], 0]).transpose(), theta), 0)
+        variables.solar_system.mass_needed_launch(position_final_AU , test = True)
