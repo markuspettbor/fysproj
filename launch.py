cf7b46e8144c745df250feff9286110b9251c89d
kjetil <kjetil_gulli@live.no>
2018-10-11 11:08:22 +0200

    launch v2, variables

--- launch.py
+++ launch.py
@@ -4,7 +4,53 @@ import numpy as np
 import variables as vars
 from scipy.interpolate import interp1d
 
+def launchv2(planet_position, theta = -1/2*np.pi):#theta = -1/2*np.pi #launchsite on planet, might want to correct for phi rotation before setting theta.
+    radius = vars.radius_normal_unit[0]
+    planet_mass = vars.m_normal_unit[0]
+    grav_const = vars.gravitational_constant
+    satellite_mass = vars.satellite
+    period = vars.period[0] #unit is 24h
+    force_box = np.load('saved/engine/force_box.npy')
+    fuel_consumed_box_per_sec = np.load('saved/engine/fuel_consumed_box_per_sec.npy')
+    part_consumed_box = fuel_box/vars.molecule_mass
+    position = radius
+    force = 35000e3 #input? kraft for hele raketten
+    fuel_mass = 3000e3-satellite_mass #input? total fuelmasse for raketten
+    boxes = force/force_box #antall bokser regnes utifra ønsket kraft
+    fuel_consumption = boxes * fuel_box #drivstofforbruk regnes ut for samlet motor
+    mass = satellite_mass + fuel_mass + rocket_mass
+    initial_mass = mass
+    initial_fuel_mass = fuel_mass
+    t = 0
+    escape_velocity = (2*grav_const*planet_mass/position)**0.5
+    velocity = 0; count = 0; has_fuel = 1
+    while velocity < escape_velocity: #1Dimentional
+        acceleration = force/mass*has_fuel - grav_const*planet_mass/(position**2)
+        velocity = velocity + acceleration*dt
+        position = position + velocity*dt
+        mass = mass - fuel_consumption*dt
+        fuel_mass = fuel_mass - fuel_consumption*dt
+        if count % 100 == 0:
+            plt.scatter(t, acceleration)
+        escape_velocity = (2*grav_const*planet_mass/position)**0.5
+        if fuel_mass < 0:
+            has_fuel = 0
+            print('NO ORBIT')
+            break
+        t = t + dt
+        count += 1
+    t_AU = t/vars.year
+    phi = Dt*np.pi/(period*24*60*60) #rotasjon til planet om seg selv
+    position_before_rotation = position * planet_position/nt.norm(planet_position) #radially outwards from sun/cm
+    velocity_before_rotation = velocity * planet_position/nt.norm(planet_position) #radially outwards from sun/cm
+    position_after_rotation = nt.rotate(position_before_rotation, phi + theta) #returns array with [x, y]
+    velocity_after_rotation = nt.rotate(velocity_before_rotation, phi + theta)
+    return t_AU, position_after_rotation, velocity_after_rotation, mass, fuel_mass
+
+
 def launch(force_box, fuel_box, testing = False):
+    '''values that shoulde become inputs:
+    t0_AU, force?, fuel_mass?, satellite_mass?, dt?, (t_load, x_load, v_load)??, theta = -1/2np.pi'''
     radius = vars.radius_normal_unit[0]
     planet_mass = vars.m_normal_unit[0]
     grav_const = vars.gravitational_constant
@@ -13,9 +59,11 @@ def launch(force_box, fuel_box, testing = False):
     position = radius
     rocket_mass = 0 #input?
     force = 35000e3 #input? kraft for hele raketten
-    fuel_mass = 3000e3 #input? total fuelmasse for raketten
+    fuel_mass = 3000e3-satellite_mass #input? total fuelmasse for raketten
+    print(fuel_mass/satellite_mass, 'MASS RATIO')
     dt = 0.01 #input? #tidssteg
-    t0 = 0*365*24*60*60 #input? starttid for launch
+    t0_AU = 0.054
+    t0 = t0_AU*365*24*60*60 #input? starttid for launch
     #Phi settes etter Dt er funnet
     theta = -1/2*np.pi #launchsite on planet
     boxes = force/force_box #antall bokser regnes utifra ønsket kraft
@@ -97,6 +145,7 @@ def launch(force_box, fuel_box, testing = False):
     launchtime_AU = Dt/(vars.year)
     print('timestep [min]', (t_load[1]-t_load[0])*365*24*60)
     print('FINAL POS AU', position_final_AU)
+    print('FINAL VEL AU/YR', velocity_final_AU)
     if testing == True:
         vars.solar_system.engine_settings(force_box, boxes, part_consumed_box, initial_fuel_mass, \
         t, nt.rotate(np.array([vars.x0[0] + vars.radius_AU[0], 0]).transpose(), theta), 0)
