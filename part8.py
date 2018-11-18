74bd385385d1cf5f5b9332f528d63d3935fede9e
Markus <markus_er_nr3@hotmail.com>
2018-11-18 14:40:33 +0100

    :=

--- part8.py
+++ part8.py
@@ -3,4 +3,5 @@ import variables as vars
 solar_system = vars.solar_system
 chosen_planet = 1
 
-solar_system.part2A_4(chosen_planet, friend_seed=None, increase_height=False, filename1="part2A4_frame1.xml", filename2="part2A4_frame2.xml")
+#solar_system.part2A_4(chosen_planet, friend_seed=None, increase_height=False, filename1="part2A4_frame1.xml", filename2="part2A4_frame2.xml")
+#solar_system.part2A_5(chosen_planet,friend_seed=None, filename1="part2A5_frame1.xml", filename2="part2A5_frame2.xml")
