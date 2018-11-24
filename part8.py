60b59c6a49ee04c83bb0e2b91be3f9e955eebb24
Markus <markus_er_nr3@hotmail.com>
2018-11-24 20:03:06 +0100

    part8

--- part8.py
+++ part8.py
@@ -5,3 +5,4 @@ chosen_planet = 1
 
 #solar_system.part2A_4(chosen_planet, friend_seed=None, increase_height=False, filename1="part2A4_frame1.xml", filename2="part2A4_frame2.xml")
 #solar_system.part2A_5(chosen_planet,friend_seed=None, filename1="part2A5_frame1.xml", filename2="part2A5_frame2.xml")
+solar_system.part2B_3(chosen_planet,friend_seed=None, filename1="part2B3_frame1.xml", filename2="part2B3_frame2.xml")
